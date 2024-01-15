bf138bee53ef335e7212c8d91f20f4065fc4f7d0
Mohammed <mnashat.dev@gmail.com>
2024-01-15 16:20:37 +0200

    create week_2

---
+++ week_2.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <limits>
+#include<array>
+#include<cstring>
+
+using namespace std;
+
+int main(){
+
+ /*
+
+
+Assignment 01
+  cout << "My Age Is: " << age << "\n";
+  cout << period << " Years Ago My Age Was: " << age - period << "\n";
+   cout << "After " << period << "Years My Age Will Be:" << period + age << "\n";
+
+
+
+Assignment 02
+  int a,b,c;
+  a = 10, b = 20, c = 70;
+  cout << "Sum Of All Number Is: " << a + b + c;
+
+Assignment 03
+
+  name        [ Valid | Good Practice ]
+  NAME        [ Valid | Bad Practice ]
+  1name       [ Not Valid ] => Reason start using number
+  __name      [ Valid ] => 
+  name@name   [ Not Valid ] => Reason  symbol is not allowed
+  name10name  [ Valid ] => 
+  name!name   [Not Valid ] => Reason Special characters like "!" are not allowed
+  first_NAME  [ Valid ] => 
+  first_name  [ Valid ] => 
+  firstName   [ Valid ] => 
+  first name  [ Not Valid ] => Reason names cannot contain spaces
+  fn          [ Valid ] => 
+  public      [ Not Valid ] => Reason  is a keyword 
+  Public      [ Valid ] => 
+
+Assignment 04
+
+  string my_text = "============\n== Elzero ==\n============\n";
+  cout << my_text;
+
+Assignment 05
+ int a, b, c = 100;
+   b=100; a=100;
+  cout << a + b + c; 
+
+Assignment 06
+
+  int a = 210, b = -50, c = -40;
+    cout << a-c-b;
+
+Assignment 07
+ int current_year;
+   int birth_year;
+  current_year = 2022;
+  birth_year = 1982;
+  int age_in_years = current_year - birth_year;
+  cout << age_in_years;
+
+
+Assignment 09
+  int inPut;
+  cin >> inPut;
+  cout << inPut * inPut;
+
+Assignment 10
+  int inPut1;
+  int inPut2;
+  cin >> inPut1;
+  cin >> inPut2;
+  cout << inPut1 * inPut2;
+
+
+Assignment 11
+  cout << "Bello\rHello Elzeroo\b";
+  cout << " Web Schoolll\b\b";
+  cout << " I Love Programming";
+  cout << " Too Much";
+  cout << " Specially C++\n";
+
+   << "\n";
+
+*/
+
+cout << "\n\n\n";
+
+
+    return 0;
+}
