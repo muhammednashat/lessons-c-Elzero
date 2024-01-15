bc5bd1e0fc1eaeb5de6e8a5d6da2840890492563
Mohammed <mnashat.dev@gmail.com>
2024-01-15 13:09:17 +0200

    week_1

---
+++ week_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <limits>
+#include<array>
+#include<cstring>
+
+using namespace std;
+
+int main(){
+    
+/*
+
+Assignment 01
+  cout << "Line One\n";
+  cout << "Line Two\n";
+  cout << "Line Three\n\n";
+
+Assignment 02
+cout << "Iam Learning Programming\nWith\nElzero\nWeb\nSchool\n<3\n\n";
+
+
+Assignment 03
+// cout << "|=======================|\n|== Elzero Web School ==|\n|==        C++        ==|\n|=======================|\n\n";
+
+*/
+
+cout << "\n\n\n";
+
+    return 0;
+}
